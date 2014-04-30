c46d03ed081df6e313831fe71124f0bdf00e37c5
Jade <jade.applegate@gmail.com>
2014-04-29 18:43:39 -0700

    second commit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,25 +4,28 @@ class UsersController < ApplicationController
   end
 
   def create
-   @user = User.new(user_params)
-  if @user.save 
-    flash[:success] = "Welcome to the Cook Book app!"
-    sign_in @user
-    redirect_to @user
-  else
-    flash[:error] = "Failed to create account.  Try again."
-    redirect_to new_user_path
-  end
-end
+     @user = User.new(user_params)
+    if @user.save 
+      flash[:success] = "Welcome to the Cook Book app!"
+      sign_in @user
+      redirect_to @user
+    else
+      flash[:error] = "Failed to create account.  Try again."
+      redirect_to new_user_path
+    end
+    end
 
-  def edit 
+    def update
+      @user = User.find(params[:id])
+      @user.update
   end
 
-  def update
+  def show
+    @user = User.find(params[:id])
   end
-
-  def destroy
-  end
+  
+  # def destroy
+  # end
 
     private
     def user_params
